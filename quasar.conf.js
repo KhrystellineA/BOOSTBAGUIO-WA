7ef52a74dafbeb4669089b902842fe0c77fbef01
Khrystelline <khrystellinea@gmail.com>
2025-11-25 00:26:59 +0800

    2

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,4 +1,182 @@
-boot: [
-  'supabase',
-  // other boot files...
-],
+// Full config: https://quasar.dev/quasar-cli/quasar-conf-js
+module.exports = function (ctx) {
+  return {
+    // ... other config
+    
+    // app boot file (/src/boot)
+    // --> boot files are part of "main.js"
+    // https://quasar.dev/quasar-cli/cli-documentation/boot-files
+    boot: [
+      'supabase',
+      // other boot files...
+    ],
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
+    css: [
+      'app.scss'
+    ],
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-extras
+    extras: [
+      // 'ionicons-v4',
+      // 'mdi-v5',
+      // 'fontawesome-v5',
+      // 'eva-icons',
+      // 'themify',
+      // 'line-awesome',
+      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+
+      'roboto-font', // optional, you are not bound to it
+      'material-icons' // optional, you are not bound to it
+    ],
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
+    framework: {
+      config: {},
+
+      // iconSet: 'material-icons', // Quasar icon set
+      // lang: 'en-us',
+
+      // For special cases outside of where the auto-import strategy can have an impact
+      // (like functional components as one of the examples),
+      // you can manually specify Quasar components/directives to be available everywhere:
+      //
+      // components: [],
+      // directives: [],
+
+      // Quasar plugins
+      plugins: ['Notify']
+    },
+
+    // https://quasar.dev/quasar-cli/cli-documentation/supporting-ts
+    supportTS: false,
+
+    // https://quasar.dev/quasar-cli/cli-documentation/prefetch-feature
+    // preFetch: true,
+
+    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
+    devServer: {
+      https: false,
+      port: 8080,
+      open: true // opens browser window automatically
+    },
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
+    build: {
+      scopeHoisting: true,
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      showProgress: true,
+      gzip: false,
+      analyze: false,
+      preloadChunks: true,
+      extractCSS: false,
+
+      // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
+      pwa: {
+        workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+        workboxOptions: {
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+          skipWaiting: true,
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/api\.boostbaguio\.ph\//,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'api-cache',
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 24 * 60 * 60 // 1 day
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/tiles\.boostbaguio\.ph\//,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'map-tiles',
+                expiration: {
+                  maxEntries: 1000,
+                  maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            }
+          ]
+        },
+
+        // for manifest.json
+        manifest: {
+          name: 'Boost Baguio',
+          short_name: 'BoostBaguio',
+          description: 'Your ultimate guide to navigating Baguio City',
+          display: 'standalone',
+          orientation: 'portrait',
+          background_color: '#FFFFFF',
+          theme_color: '#2E5D3E',
+          icons: [
+            {
+              src: 'icons/icon-128x128.png',
+              sizes: '128x128',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-256x256.png',
+              sizes: '256x256',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-384x384.png',
+              sizes: '384x384',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      }
+    },
+
+    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
+    devServer: {
+      https: false,
+      port: 8080,
+      open: true // opens browser window automatically
+    },
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
+    framework: {
+      // all boot files execute by default
+      boot: [
+        'supabase'
+      ],
+
+      // all CSS Stylus files in "quasar-variables" and "quasar-variables-extended"
+      css: [
+        'app.styl'
+      ],
+
+      // Quasar plugins
+      plugins: ['Notify']
+    },
+
+    // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
+    supportIE: false,
+
+    // https://quasar.dev/quasar-cli/cli-documentation/supporting-ts
+    supportTS: false,
+
+    // https://quasar.dev/quasar-cli/cli-documentation/prefetch-feature
+    // preFetch: true
+  }
+}
